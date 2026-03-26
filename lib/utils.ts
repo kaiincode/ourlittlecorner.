4382f4088451b9755ddbf74270a4f2ec382c55b1
kaiincode <ng.t.thien01@gmail.com>
2026-03-26 11:01:46 +0700

    Update package.json and pnpm-lock.yaml to reintroduce Next.js version 15.2.8. Refactor utility exports in utils.ts for improved organization and accessibility of date and rich text functions.

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export {
+  formatLocalDate,
+  formatLocalTime,
+  formatLocalDateTime,
+  formatTimelineDate,
+} from "./utils/dateUtils"
+
+export {
+  stripHtml,
+  sanitizeRichTextHtml,
+  contentHasFontStyling,
+  htmlToDisplayHtml,
+  htmlToEditorHtml,
+} from "./utils/richText"
